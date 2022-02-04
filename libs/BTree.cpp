364dd362fa444ea1a8bc514225d2aba90061ad1a
Nima-Niroumand <nimaniroumand80@gmail.com>
2022-02-04 22:14:03 +0330

    nima2

--- libs/BTree.cpp
+++ libs/BTree.cpp
@@ -54,119 +54,122 @@ T BTree<T>::max(T a, T b) {
 }
 template <typename T>
 BTNode<T> *BTree<T>::s(BTNode<T> *h, T k, int in, BTNode<T> *p) {
-    if (h != NULL) {
-        if (h->l) return h;
-        for (int i = 0; i < h->num; i++) {
-            if (k < h->a[i]) {
-                return s(h->b[i], k);
+    if( h != NULL){
+        if (h->l) return h ;
+        int i ;
+        for( i = 0 ; i <h->Num ; i++){
+            if (k < h->a[i]){
+                return s(h->b[i] , k , i , h) ;
             }
         }
-        return s(h->b[h->num], k);
-    } else {
-        BTNode<T> *tnode = new BTNode<T>(m, true);
-        p->b[in] = tnode;
-        tnode->parent = p;
+        return s(h->b[h->Num] , k , i , h) ;
+    }
+    else{
+        BTNode<T> *tnode = new BTNode<T>( m , true);
+        p->b[in] = tnode ;
+        tnode->parent = p ;
         return tnode;
     }
 }
 template <typename T>
 BTNode<T> *BTree<T>::insert(T k) {
-    if (root == NULL) {
-        BTNode<T> *tnode = new BTNode<T>(m, true);
-        r = tnode;
-        r->parent = NULL;
-        r->a[0] = k;
-        r->Num++;
-        return r;
-    } else {
-        if (root->Num == m - 1) {
-            BTNode<T> *tnode = new BTNode<T>(m, false);
-            tnode->b[0] = root;
-        }
-        /*BTNode* h = s(r, k, 0 , NULL);
+    if( root == NULL ){
+        BTNode<T> *tnode = new BTNode<T>( m , true);
+        root = tnode ;
+        root->parent = NULL ;
+        root->a[0] = k ;
+        root->Num ++ ;
+        return root ;
+    }
+    else{
+        /*if( root->Num == m-1){
+            BTNode<T> *tnode = new BTNode<T>( m , false);
+            tnode->b[0] = root ;
+        }*/
+        BTNode<T> * h = s(root, k, 0 , NULL);
         while(h != NULL ){
 
-                if(h->Num == 0 ){
-                        h->a[0] = k ;
-                        h->Num ++ ;
-                        return r ;
+            if(h->Num == 0 ){
+                h->a[0] = k ;
+                h->Num ++ ;
+                return root ;
+            }
+            if(h->Num < m-1 ){
+                int i = 0 ;
+                while( h->a[i] < k && i<h->Num) i++;
+                for( int j = h->Num - 1 ; i<=j ; j--){
+                    h->a[j+1] = h->a[j] ;
                 }
-                if(h->Num < m-1 ){
-                        int i = 0 ;
-                        while( h->a[i] < k && i<h->num) i++;
-                        for( int j = h->Num - 1 ; i<=j ; j--){
-                                h->a[j+1] = h->a[j] ;
-                        }
-                        h->a[i] = k ;
-                        h->Num ++;
-                        return r ;
+                h->a[i] = k ;
+                h->Num ++;
+                return root ;
 
+            }
+            if( h->Num == m-1 && h->parent != NULL && h->parent->Num < m){
+                BTNode<T>* q = NULL;
+                int in ;
+                for ( int i = 0 ; i <h->parent->Num ; i++){
+                    if(h->parent->b[i] == h){
+                        in = i ;
+                        break;
+                    }
                 }
-                if( h->Num == m-1 && h->parent != NULL && h->parent->Num < m){
-                        BTNode* q = NULL;
-                        int in ;
-                        for ( int i = 0 ; i <h->parent->Num ; i++){
-                                if(h->parent->b[i] == h){
-                                        in = i ;
-                                        break;
-                                }
-                        }
-                        if(in <m-1){
-                                q = h->parent->b[in + 1] ;
-                                if (q == NULL){
-                                        int p1;
-                if (in == 0 ) p1 = h->parent->a[0];
-                else p1 = h->parent->a[in - 1];
-                for (int i = (q->n) - 1; j >= 1; j--)
-                q->key[j + 1] = q->key[j];
-                q->key[0] = put;
-                p->parent->key[m == 0 ? m : m - 1] = p->key[p->n - 1];
-                                }
-                                else{
-                                        if(q->Num == m-1){
-                                                int * merge ;
-                                                merge = new int [2*m] ;
-                                                int i ;
-                                                for( i = 0 ; i < h->Num ; i++){
-                                                        merge[i] = h->a[i] ;
-                                                }
-                                                merge[i+1] = k ;
-                                                merge[i+2] = h->parent->a[in] ;
-                                                for( int j = i+2 ; j < i + 2 + q->Num ; j++ ){
-                                                        merge[j] = q->a[j-i-2] ;
-                                                }
-                                                BTNode<T> *s = new BTNode<T>( m , true) ;
-                                                int p1 , p2 ;
-                                                for (int i = 0; i < (2*m-2) / 3; i++) h->a[i] = merge[i];
-                        p1 = merge[(2*m-2) / 3];
-                        for (int i = (2*m-2) / 3 + 1 ; i < (4*m)/ 3; i++) q->a[i - (2*m-2) / 3 - 1 ] = merge[i];
-                        p2 = merge[(4*m)/ 3];
-                        for (int i = (4*m) / 3 + 1 ; i < (2*m); i++) s->a[i - (4*m) / 3 - 1 ] = merge[i];
-                        if( ind==0 || ind==1){
+                if(in <m-1){
+                    q = h->parent->b[in + 1] ;
+                    if (q == NULL){
+                        int p1;
+                        if (in == 0 ) p1 = h->parent->a[0];
+                        else p1 = h->parent->a[in - 1];
+                        for (int j = (q->Num) - 1; j >= 1; j--)
+                            q->a[j + 1] = q->a[j];
+                        q->a[0] = p1;
+                        h->parent->a[m == 0 ? m : m - 1] = h->a[h->Num - 1];
+                    }
+                    else{
+                        if(q->Num == m-1){
+                            int * merge ;
+                            merge = new int [2*m] ;
+                            int i ;
+                            for( i = 0 ; i < h->Num ; i++){
+                                merge[i] = h->a[i] ;
+                            }
+                            merge[i+1] = k ;
+                            merge[i+2] = h->parent->a[in] ;
+                            for( int j = i+2 ; j < i + 2 + q->Num ; j++ ){
+                                merge[j] = q->a[j-i-2] ;
+                            }
+                            BTNode<T> *s = new BTNode<T>( m , true) ;
+                            int p1 , p2 ;
+                            for (int i = 0; i < (2*m-2) / 3; i++) h->a[i] = merge[i];
+                            p1 = merge[(2*m-2) / 3];
+                            for (int i = (2*m-2) / 3 + 1 ; i < (4*m)/ 3; i++) q->a[i - (2*m-2) / 3 - 1 ] = merge[i];
+                            p2 = merge[(4*m)/ 3];
+                            for (int i = (4*m) / 3 + 1 ; i < (2*m); i++) s->a[i - (4*m) / 3 - 1 ] = merge[i];
+                            if( in==0 || in==1){
                                 h->parent->a[0] = p1;
-                        h->parent->a[1] = p2;
-                        h->parent->b[0] = h;
-                        h->parent->b[1] = q;
-                        h->parent->b[2] = s;
-                        return r;
-                                                }
-                                                else{
-                                                        h->parent->a[ind - 1] = p1;
-                        h->parent->a[ind] = p2;
-                        h->parent->b[ind-1] = h;
-                        h->parent->b[ind] = q;
-                        h->parent->b[ind + 1] = s;
-                        return r;
-                                                }
+                                h->parent->a[1] = p2;
+                                h->parent->b[0] = h;
+                                h->parent->b[1] = q;
+                                h->parent->b[2] = s;
+                                return root;
+                            }
+                            else{
+                                h->parent->a[in - 1] = p1;
+                                h->parent->a[in] = p2;
+                                h->parent->b[in-1] = h;
+                                h->parent->b[in] = q;
+                                h->parent->b[in + 1] = s;
+                                return root;
+                            }
 
 
-                                        }
-
-                                }
                         }
+
+                    }
                 }
-        h = h->parent ;
-        }*/
+            }
+            h = h->parent ;
+        }
     }
 }
 
