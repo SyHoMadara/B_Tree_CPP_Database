bd878c7464d8dc5d42a1fd90754a3ddddfdd3e60
SyHoMadara <sh.sarafrazi80@gmail.com>
2022-02-05 18:08:15 +0330

    syntax problem

--- libs/BTree.h
+++ libs/BTree.h
@@ -1,18 +1,23 @@
 #include <cmath>
 #include <iostream>
+
 using namespace std;
-template< typename T >class Node{
+template<typename T>
+class BTNode;
+template<typename T>
+class Node {
 public:
     T data;
-    Node<T>* nextField;
-    BTNode<T>* self;
+    Node<T> *nextField;
+    BTNode <T> *self;
 };
 
-template <typename T>
+template<typename T>
 class BTree;
-template <typename T>
+
+template<typename T>
 class BTNode {
-   public:
+public:
     int m;
     int min;
     Node<T> *a;
@@ -20,27 +25,37 @@ class BTNode {
     BTNode<T> *parent;
     int Num;
     bool l;
+
     BTNode(int M, bool f);
+
     bool IsFull();
+
     BTNode<T> *search(int k);
+
     void print();
 };
 
-template <typename T>
+template<typename T>
 class BTree {
-   private:
+private:
     BTNode<T> *root;
     int m;
     int min;
     int flag, flag1;
 
-   public:
+public:
     explicit BTree(int M);
+
     T max(T a, T b);
+
     BTNode<T> *s(BTNode<T> *h, T k, int in, BTNode<T> *p);
-    void f(BTNode<T> * h ,int x ,BTNode<T> * t) ;
-    void insert1(BTNode<T> * h , T k) ;
-    void insert(T k );
+
+    void f(BTNode<T> *h, int x, BTNode<T> *t);
+
+    void insert1(BTNode<T> *h, T k);
+
+    void insert(T k);
+
     void print();
 
     BTNode<T> *Root();
