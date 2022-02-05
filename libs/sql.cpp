91c1bfc5f215f89b0af388f7611231de9f6e2b76
SyHoMadara <sh.sarafrazi80@gmail.com>
2022-02-05 20:38:54 +0330

    extract parameter from command

--- libs/sql.cpp
+++ libs/sql.cpp
@@ -1,7 +1,10 @@
+#include <regex>
 #include "sql.h"
 
-sql::sql(){
-    commands = {"CREATE", "DELETE", "UPDATE", "INSERT", "SElECT"};
+
+sql::sql() {
+    commands = {"CREATE TABLE", "DELETE FROM", "UPDATE", "INSERT INTO", "SElECT"};
+    for (auto &command : commands) command += "(.+)";
 }
 
 vector<string> sql::tokenize(string &str) {
@@ -14,7 +17,7 @@ vector<string> sql::tokenize(string &str) {
 
 pair<int, string> *sql::find_command(string &command) {
     for (int i = 0; i < commands.size(); i++)
-        if (command == commands[i])
+        if (regex_match(command, regex(commands[i])))
             return new pair<int, string>(i, command);
     return nullptr;
 }
@@ -46,6 +49,8 @@ void sql::remove(const string &command) {
 }
 
 void sql::create(const string &command) {
+    //CREATE TABLE {table name} (column1 type,column2 type,...)
+
 
 }
 
@@ -61,8 +66,56 @@ void sql::select(const string &command) {
 
 }
 
+string sql::conlong2str(long long a){
+
+}
+
+long long sql::constr2long(string s){
+
+}
+
+string sql::contime2str(long long a){
+
+}
+
+long long sql::constr2time(string s){
+
+}
+
+NODE_HASH_TYPE sql::hash_code(const string& s1, const string& s2){
+    NODE_HASH_TYPE result;
+    if(s2=="int"){
+        result.first = stoi(s1);
+        result.second = 0;
+    } else if(s2=="string"){
+        result.first = constr2long(s1);
+        result.second = 1;
+    } else if(s2=="time"){
+        result.first = constr2time(s1);
+        result.second = 2;
+    }
+    return result;
+}
+
+vector<NODE_HASH_TYPE> sql::extract_parameters(const string &par) {
+    string clear_par;
+    regex par_regex(R"([\w\d]+ [\w]+)"), r(R"([\w\d]+)");
+    for (auto it = par.begin() + 1; it != par.end() - 1; it++)
+        clear_par += *it;
+    vector<NODE_HASH_TYPE> result;
+    for(sregex_iterator it(clear_par.begin(), clear_par.end(), par_regex), it_end; it!=it_end; it++){
+        string ss = it->str();
+        sregex_iterator it2(ss.begin(), ss.end(), r);
+        result.push_back(hash_code(it->str(), (++it)->str()));
+    }
+    return result;
+}
+
+
+
 
 //sql& sql::get_sql() {
 //    if(sql::SQL == nullptr) sql::SQL = new sql();
 //    return *sql::SQL;
 //}
+
--- libs/sql.h
+++ libs/sql.h
@@ -9,22 +9,44 @@
 
 using namespace std;
 
+using NODE_HASH_TYPE=pair<long long, int>;
 class sql {
 protected:
     explicit sql();
+
 public:
 //    static sql* SQL;
-    static sql& get_sql();
+//    static sql& get_sql();
     const vector<data_base> dbs;
     vector<string> commands;
+
     static vector<string> tokenize(string &str);
-    pair<int, string>* find_command(string &command);
+
+    pair<int, string> *find_command(string &command);
+
     void command_handler(string command);
+
+    static vector<pair<long long int, int>> extract_parameters(const string &par);
+
     void remove(const string &command);
+
     void create(const string &command);
+
     void update(const string &command);
+
     void insert(const string &command);
+
     void select(const string &command);
+
+    static NODE_HASH_TYPE hash_code(const string &s1, const string &s2);
+
+    string conlong2str(long long int a);
+
+    static long long int constr2long(string s);
+
+    string contime2str(long long int a);
+
+    static long long int constr2time(string s);
 };
 
 
