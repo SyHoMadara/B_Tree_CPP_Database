47e7ae810bcab100156c0336f7dc1e06036bb3cc
SyHoMadara <sh.sarafrazi80@gmail.com>
2022-02-07 11:23:51 +0330

    clean

--- libs/BTree.h
+++ libs/BTree.h
@@ -1,20 +1,25 @@
 #include <cmath>
 #include <iostream>
+
 using namespace std;
-template <typename T>
+
+template<typename T>
 class BTNode;
-template< typename T >class Node{
+
+template<typename T>
+class Node {
 public:
     T data;
-    Node<T>* nextField;
-    BTNode<T>* self;
+    Node<T> *nextField;
+    BTNode<T> *self;
 };
 
-template <typename T>
+template<typename T>
 class BTree;
-template <typename T>
+
+template<typename T>
 class BTNode {
-   public:
+public:
     int m;
     int min;
     Node<T> *a;
@@ -22,28 +27,40 @@ class BTNode {
     BTNode<T> *parent;
     int Num;
     bool l;
+
     BTNode(int M, bool f);
+
     bool IsFull();
+
     BTNode<T> *search(int k);
+
     void print();
 };
 
-template <typename T>
+template<typename T>
 class BTree {
-   private:
-    BTNode<T> * root ;
+private:
+    BTNode<T> *root;
     int m;
     int min;
     int flag, flag1;
 
-   public:
+public:
     string name;
-    explicit BTree(int M, const string& name);
+    string type;
+
+    explicit BTree(int M, const string &name, string type);
+
     T max(T a, T b);
+
     BTNode<T> *s(BTNode<T> *h, T k, int in, BTNode<T> *p);
-    void f(BTNode<T> * h ,int x ,BTNode<T> * t) ;
-    Node<T>* insert1(BTNode<T> * h , T k) ;
-    Node<T>* insert(T k );
+
+    void f(BTNode<T> *h, int x, BTNode<T> *t);
+
+    Node<T> *insert1(BTNode<T> *h, T k);
+
+    Node<T> *insert(T k);
+
     void print();
 
     BTNode<T> *Root();
